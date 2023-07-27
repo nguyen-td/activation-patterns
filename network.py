268fd90231d29b88b9c4c3321ae166a058a7d2e7
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-07-27 20:27:42 +0200

    convert to numpy properly

--- network.py
+++ network.py
@@ -196,7 +196,7 @@ class RNNModel(nn.Module):
 
             # get prediction
             y_list = [item for sublist in y_pred for item in sublist]
-            y_pred = np.array(y_list[:n_data])
+            y_pred = y_list[:n_data].detach().cpu().numpy()
 
         return aggregate_loss, y_pred
 
