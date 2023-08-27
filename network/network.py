1cbdc212b90e45dfadf59adcc64c3ddfc5afd6d1
Hiyeri <nguyen.tiendung.otten@gmail.com>
2023-08-27 19:23:56 +0200

    try sth

--- network/network.py
+++ network/network.py
@@ -119,10 +119,11 @@ class RNNModel(nn.Module):
 
         # initialization
         x[:, 0, :] = torch.tensor(self.x0, device=self.device)
-        if self.activation == 'relu':
-            u[:, 0, :] = torch.relu(x[:, 0, :])
-        else: # tanh
-            u[:, 0, :] = torch.tanh(x[:, 0, :])
+        u[:, 0, :] = torch.relu(x[:, 0, :])
+        # if self.activation == 'relu':
+        #     u[:, 0, :] = torch.relu(x[:, 0, :])
+        # else: # tanh
+        #     u[:, 0, :] = torch.tanh(x[:, 0, :])
         
         # simulate network
         for t in range(T-1):
