119a64b38c745226514803b0366ce41454c3d71b
Hiyeri <nguyen.tiendung.otten@gmail.com>
2024-05-05 17:12:03 +0200

    change file name

--- train_network.py
+++ train_network.py
@@ -12,28 +12,28 @@ mini_batch_size = 16
 n_data = mini_batch_size * 1000
 activation = 'relu' # type of activation function
 data_type = 'sim' # "sim" or "real"
-T_real = 1 # duration in minutes, for real data
+T = 1 # duration in minutes, for real data
 lr = 1e-4 # default
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
 device = "cpu:1"
 
 # generate training data
 if data_type == 'sim':
-    T_sim = 60  # duration of simulated trajectories (seconds)
+    T = 60  # duration of simulated trajectories (seconds)
     srate = 50  # sampling rate (Hz)
 
     border_region = 0.03  # max. distance to wall (m)
-    sequence_length = T_sim * srate  # number of steps in trajectory
+    sequence_length = T * srate  # number of steps in trajectory
     box_width = 2.2       # width of training environment (m)
     box_height = 2.2      # height of training environment (m)
 
     trajectory_generator = TrajectoryGenerator(sequence_length, border_region, box_width, box_height, n_data)
     position, vel_x, vel_y = trajectory_generator.generate_trajectory() # position, velocity, head_direction
 
-    torch.save([position[0], vel_x[0], vel_y[0]], f'data\data-{activation}-{data_type}-{T_sim}.pt')
+    torch.save([position[0], vel_x[0], vel_y[0]], f'data\data-{activation}-{data_type}-{T}.pt')
 else:
-    position, vel_x, vel_y = load_moserdata(f't2c1_{T_real}min.mat', n_data)
-    torch.save([position[0], vel_x[0], vel_y[0]], f'data\data-{activation}-{data_type}-{T_real}.pt')
+    position, vel_x, vel_y = load_moserdata(f't2c1_{T}min.mat', n_data)
+    torch.save([position[0], vel_x[0], vel_y[0]], f'data\data-{activation}-{data_type}-{T}.pt')
     
 train = make_train_data(vel_x, vel_y)
 
@@ -41,7 +41,7 @@ train = make_train_data(vel_x, vel_y)
 n_epochs = 10
 hidden_size = 256
 rnn_layer = 'custom'
-model_name = f'RNN-{hidden_size}-{rnn_layer}-{activation}-{data_type}'
+model_name = f'RNN-{hidden_size}-{rnn_layer}-{activation}-{data_type}-{T}'
 
 trainer = Trainer(device, train, position, model_name, hidden_size=hidden_size, mini_batch_size=mini_batch_size, rnn_layer=rnn_layer, n_epochs=n_epochs, activation=activation)
 train_loss_epochs = trainer.train()
@@ -53,9 +53,6 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.savefig('loss.png', bbox_inches='tight')
 
-if data_type == 'sim':  
-    torch.save(train_loss_epochs, f'models\loss-{activation}-{data_type}-{T_sim}.pt')
-else:
-    torch.save(train_loss_epochs, f'data\data-{activation}-{data_type}-{T_real}.pt')
+torch.save(train_loss_epochs, f'models/loss-{activation}-{data_type}-{T}.pt')
 
 # plt.show()
